6eb5ba6befe4c9517b8e3424619e286ee7559682
Soka <soka@cnous.fr>
2016-10-27 18:19:12 +0200

    stable works

--- openfisca_cnous/bourse.py
+++ openfisca_cnous/bourse.py
@@ -5,9 +5,10 @@ from numpy import (maximum as max_, logical_not as not_, absolute as abs_, minim
 
 from openfisca_france.model.base import *  # noqa analysis:ignore
 
-from openfisca_cnous import plafonds
+from openfisca_cnous.plafonds import get_plafond    
 import ipdb
 #       ipdb.set_trace()
+import numpy as np
 
 
 class bourse(Variable): 
@@ -19,8 +20,16 @@ class bourse(Variable):
         period = period.this_month
         ## nbr d'enfants enseignements supétrieur
         nbr_siblings_cycle_sup = individu.foyer_fiscal('f7ef', period)
+
+        # cast foyer fiscal -> individu
+        nbr_siblings_cycle_sup = individu.foyer_fiscal.project(nbr_siblings_cycle_sup)
+
         ## total de personne
-        nbr_siblings = individu.foyer_fiscal.nb_persons(role=PERSON_A_CHARGE)
+        nbr_siblings = individu.foyer_fiscal.nb_persons(role=PERSONNE_A_CHARGE)
+
+        # cast foyer fiscal -> individu
+        nbr_siblings = individu.foyer_fiscal.project(nbr_siblings)
+
         nbr_siblings_non_cycle_sup = nbr_siblings - nbr_siblings_cycle_sup 
         #montant_par_enfant = legislation(period).ma_collectivite.mon_aide.montant
         distance = individu('distance_domicile_familial', period)
@@ -32,18 +41,20 @@ class bourse(Variable):
         avis_imposition_required = individu.foyer_fiscal('rbg', period.n_2)
 
         # calcul du point de charge
-        pc = (nbr_siblings_cycle_sup-1)*4 + nbr_siblings_non_cycle_sup*2 + select([cond_dist_0, cond_dist_1, cond_dist_2], [2,1,0])
+        pc = nbr_siblings_non_cycle_sup*2 + (nbr_siblings_cycle_sup-1)*4 #+ select([cond_dist_0, cond_dist_1, cond_dist_2], [2,1,0])
 
-        list_plafond = plafond(pc)
-        # list_plafond.reverse()
-        conditions = [avis_imposition_required <= plafond in plafond in list_plafond]
+        list_plafond = get_plafond(pc)
+        list_plafond = list_plafond[::-1]
+        conditions = [avis_imposition_required <= plafond for plafond in list_plafond]
+
+
 
         echelon = select(
           conditions,
-          range(8).reverse(),
+          range(7,-1,-1),
           -1
           )
         # liste des montants alloues aux etudiants en fonctions des criteres .
-        list_montants_aides = [1009, 1669, 2513, 3218, 3924, 4505, 4778, 5551]
+        list_montants_aides = np.asarray([1009, 1669, 2513, 3218, 3924, 4505, 4778, 5551])
 
-        return (period, list_montants_aides[echelon])
+        return (period,list_montants_aides[echelon])
--- openfisca_cnous/plafonds.py
+++ openfisca_cnous/plafonds.py
@@ -1,15 +1,18 @@
 import csv
 from os import path
+import ipdb
+import numpy as np
 
 print path.dirname(__file__)	
-plafonds = {}
+plafonds = []
 with open(path.join(path.dirname(__file__),'plafonds.csv')) as csv_file:
    csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
    #csv_reader.next()
 
    for row in csv_reader:
        pt_charge = int(row[0])
-       plafonds[pt_charge] = map(lambda x : int(x), row[1:])
+       plafonds.append(np.asarray(map(lambda x : int(x), row[1:])))
 
-def plafond(point_charge):
-   return plafonds[point_charge]
+plafonds = np.asarray(plafonds)
+def get_plafond(point_charge):
+	return np.transpose(plafonds[point_charge.astype(int)])
